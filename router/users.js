e1c58627de92e9a3a2b178f6b3aa87d153594e27
Nijarudeen <nijarr2020@gmail.com>
2020-04-16 01:12:53 +0530

    login and register form rendered

--- router/users.js
+++ router/users.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 //Login
 router.get('/login', (req, res) => {
-  res.send('Login');
+  res.render('login');
 })
 
 //Register
 router.get('/register', (req, res) => {
-  res.send('Register');
+  res.render('register');
 })
 
 module.exports = router;
