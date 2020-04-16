db299e3e52759932b92b0f20232a0089b9fe7f8d
Nijarudeen <nijarr2020@gmail.com>
2020-04-16 16:12:46 +0530

    user registered

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MongoURI: 'mongodb+srv://node:node@authentication-tqsrs.mongodb.net/test?retryWrites=true&w=majority'
+  MongoURI: `mongodb+srv://nijar:nijar@auth-rrkdn.mongodb.net/test?retryWrites=true&w=majority`
 
 }
--- models/User.js
+++ models/User.js
@@ -21,4 +21,4 @@ const UserSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", UserSchema);
 
-module.export = User;
+module.exports = User;
