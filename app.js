7619bd68effe7a4c156df5e084e61e401377d1f9
Nijarudeen <nijarr2020@gmail.com>
2020-04-16 13:25:11 +0530

    register validation added

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const express = require("express");
-const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
 const app = express();
 
 // Config DB
-const db = require('./config/keys').MongoURI;
+const db = require("./config/keys").MongoURI;
 
 // Connect to Mongo
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 // EJS
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
+  
 //Routes
-app.use("/", require("./router/index"));
-app.use("/users", require("./router/users"));
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
 
 const PORT = process.env.PORT || 5000;
 
---
+++ routes/users.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+
+//Login
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+//Register
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.post("/register", (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  console.log(name, email, password, password2);
+  var errors = [];
+  // Validation
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "Please fill in all fields" });
+  }
+
+  // Check password match
+  if (password !== password2) {
+    errors.push({ msg: "Pass do not match" });
+  }
+
+  // Check password length
+  if (password.length < 6) {
+    errors.push({ msg: "Password should be at least 6 characters" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2,
+    });
+  } else {
+    res.send("pass");
+  }
+});
+
+module.exports = router;
